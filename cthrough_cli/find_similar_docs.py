d9fc363dc1fb49376109e1749fb9ca0dd80e9411
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-01-05 19:43:46 +0530

    Add new cli handler

--- cthrough_cli/find_similar_docs.py
+++ cthrough_cli/find_similar_docs.py
@@ -12,4 +12,28 @@ def find_similar_docs(primary_doc, list_of_docs, threshold=None, pretty=True):
 	if not response:
 		print ("Operation could not be completed successfully.")
 		return 1
-	
+
+	if len(response.get("data")) == 0:
+		print ("No documnets similar to {} found".format(primary_doc.get("name")))
+		return 0
+
+	if not pretty:
+		print ("Documents similar to {}:".format(primary_doc.get("name")))
+		print ("\n")
+		for doc in response.get("data"):
+			print (doc.get("name"), doc.get("score"))
+		return 0
+
+	table_header = ["Document Name", "Similarity score"]
+	table_body = []
+
+	for doc in response.get("data"):
+		table_body.append([doc.get("name"), doc.get("score")])
+
+	print ("Documents similar to {}:".format(primary_doc.get("name")))
+	print ("\n")
+	print (tabulate(table_body, table_header, tablefmt="orgtbl"))
+	print ("\n")
+
+	return 0
+
