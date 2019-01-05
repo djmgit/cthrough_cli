2159f4c207a1a18d99168bada006d6d0e478b4c6
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-01-05 21:17:02 +0530

    Add cli handler

---
+++ cthrough_cli/cluster_images.py
@@ -0,0 +1,36 @@
+from api_handler import api_handler
+import tabulate
+from apis import cluster_images_api
+
+def cluster_docs(list_of_images, threshold=None, pretty=True):
+	request_body = {
+		"list_of_images": list_of_images,
+		"threshold": threshold
+	}
+
+	response = api_handler(url, request_body)
+	if not response:
+		print ("Operation could not be completed successfully.")
+		return 1
+
+	if not pretty:
+		print ("Clusters: ")
+		print ("\n")
+		for cluster in response.get("data"):
+			print (" ".join(cluster))
+		print ("\n")
+
+		return 0
+
+	table_header = ['Cluster ID', "Image Names"]
+	table = []
+
+	for index, cluster in enumerate(response.get("data")):
+		table.append([index] + cluster)
+
+	print ("Clusters: ")
+	print ("\n")
+	print (tabulate(table_body, table_header, tablefmt="orgtbl"))
+	print ("\n")
+
+	return 0
