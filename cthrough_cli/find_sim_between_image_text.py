b5db96b65cd4cfea6876ac6cd13b9a1895bfeaa9
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-01-05 20:33:01 +0530

    Add cli handler

---
+++ cthrough_cli/find_sim_between_image_text.py
@@ -0,0 +1,18 @@
+from api_handler import api_handler
+import tabulate
+from apis import find_sim_between_image_text_api
+
+def find_sim_between_image_text(img, doc, pretty=True):
+	request_body = {
+		"img": img,
+		"doc": doc
+	}
+
+	response = api_handler(url, request_body)
+	if not response:
+		print ("Operation could not be completed successfully.")
+		return 1
+
+	print ("Similarity score: " + response.get("data").get("score"))
+	return 0
+	
