aa147b06ba85efc895e8d41291a5fd61e0190edf
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2019-01-05 23:30:37 +0530

    Add api list

---
+++ cthrough_cli/apis.py
@@ -0,0 +1,11 @@
+# all the different endpoints
+
+find_im_between_two_api = "https://nwqhr5fk8c.execute-api.us-east-1.amazonaws.com/staging/find-sim-between-two"
+find_similar_docs_api = "https://nwqhr5fk8c.execute-api.us-east-1.amazonaws.com/staging/find-similar-docs"
+find_similar_pairs_api = "https://nwqhr5fk8c.execute-api.us-east-1.amazonaws.com/staging/find-similar-pairs"
+cluster_docs_api = "https://nwqhr5fk8c.execute-api.us-east-1.amazonaws.com/staging/cluster-docs"
+find_sim_between_two_images_api = "https://nwqhr5fk8c.execute-api.us-east-1.amazonaws.com/staging/find-sim-between-two-images"
+find_sim_between_image_text_api = "https://nwqhr5fk8c.execute-api.us-east-1.amazonaws.com/staging/find-sim-between-image-text"
+find_images_similar_to_doc_api = "https://nwqhr5fk8c.execute-api.us-east-1.amazonaws.com/staging/find-images-similar-to-doc"
+find_docs_similar_to_image_api = "https://nwqhr5fk8c.execute-api.us-east-1.amazonaws.com/staging/find-docs-similar-to-image"
+cluster_images_api = 'https://nwqhr5fk8c.execute-api.us-east-1.amazonaws.com/staging/cluster-images'
